d4e1257db8d45a52510efdc5d93d4225bd678b01
Davi Silva <davi.cunha.silva.ca@gmail.com>
2021-04-25 18:55:02 -0300

    build(package.json): test
    test

--- src/controllers/cryptocurrencies/index.ts
+++ src/controllers/cryptocurrencies/index.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import CrypticBase from 'cryptic-base';
 
+import { fetchGet } from '@utils/fetch';
+
 const crypticbase = new CrypticBase(false);
 
 export async function index(req: Request, res: Response): Promise<Response> {
@@ -10,6 +12,32 @@ export async function index(req: Request, res: Response): Promise<Response> {
     return res.status(200).send({
       status_code: 200,
       results: cryptocurrencies,
+      errors: [],
+    });
+  } catch (err) {
+    return res.status(500).send({
+      status_code: 500,
+      results: [],
+      errors: [err.message],
+    });
+  }
+}
+
+export async function indexCoinGecko(
+  req: Request,
+  res: Response,
+): Promise<Response> {
+  try {
+    const data = await fetchGet('https://api.coingecko.com/api/v3/coins/list');
+
+    const response = data.filter(
+      (crypto) => crypto.id.includes('long') === false &&
+        crypto.id.includes('short') === false,
+    );
+
+    return res.status(200).send({
+      status_code: 200,
+      results: response,
       errors: [],
     });
   } catch (err) {
@@ -43,6 +71,46 @@ export async function createCryptocurrencies(
     return res.status(500).send({
       status_code: 500,
       results: {},
+      errors: [err.message],
+    });
+  }
+}
+
+export async function createCryptocurrenciesCoinGecko(
+  req: Request,
+  res: Response,
+): Promise<Response> {
+  try {
+    const data = await fetchGet('https://api.coingecko.com/api/v3/coins/list');
+
+    const response = data.filter(
+      (crypto) => crypto.id.includes('long') === false &&
+        crypto.id.includes('short') === false,
+    );
+
+    console.log(response);
+
+    // response.forEach(async (c) => {
+    //   try {
+    //     const coinData = await fetchGet(
+    //       `https://api.coingecko.com/api/v3/coins/ethereum`,
+    //     );
+
+    //     console.log(coinData);
+    //   } catch (err) {
+    //     console.log('error');
+    //   }
+    // });
+
+    return res.status(200).send({
+      status_code: 200,
+      results: response,
+      errors: [],
+    });
+  } catch (err) {
+    return res.status(500).send({
+      status_code: 500,
+      results: [],
       errors: [err.message],
     });
   }
--- src/routes/cryptocurrencies/index.ts
+++ src/routes/cryptocurrencies/index.ts
@@ -1,12 +1,25 @@
 import { Router } from 'express';
 
-import { index, createCryptocurrencies } from '@controllers/cryptocurrencies';
+import {
+  index,
+  indexCoinGecko,
+  createCryptocurrencies,
+  createCryptocurrenciesCoinGecko,
+} from '@controllers/cryptocurrencies';
 import { authenticateUser } from '@middlewares/authorization';
 
 const router = Router();
 
 router.get('', index);
 
+router.get('/coin-gecko', indexCoinGecko);
+
 router.post('/create', authenticateUser, createCryptocurrencies);
+
+router.post(
+  '/coin-gecko/create',
+  authenticateUser,
+  createCryptocurrenciesCoinGecko,
+);
 
 export default router;
