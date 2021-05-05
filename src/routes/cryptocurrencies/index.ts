bdb799983a0747e038affe2251b520216498d6a6
Davi Silva <davi.cunha.silva.ca@gmail.com>
2021-05-05 09:06:12 -0300

    updates middlewares

--- src/routes/cryptocurrencies/index.ts
+++ src/routes/cryptocurrencies/index.ts
@@ -6,7 +6,9 @@ import {
   createCryptocurrencies,
   createCryptocurrenciesCoinGecko,
 } from '@controllers/cryptocurrencies';
+
 import { authenticateUser } from '@middlewares/authorization';
+import { validateCreateCryptocurrency } from '@middlewares/validators/request/cryptocurrencies';
 
 const router = Router();
 
@@ -14,7 +16,12 @@ router.get('', index);
 
 router.get('/coin-gecko', indexCoinGecko);
 
-router.post('/create', authenticateUser, createCryptocurrencies);
+router.post(
+  '/create',
+  authenticateUser,
+  validateCreateCryptocurrency,
+  createCryptocurrencies,
+);
 
 router.post(
   '/coin-gecko/create',
