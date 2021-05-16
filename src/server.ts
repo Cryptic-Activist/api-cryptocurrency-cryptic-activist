444f794093a0da106be6f4377979b75bb540eb31
Davi Silva <davi.cunha.silva.ca@gmail.com>
2021-05-16 16:19:13 -0300

    feat(test): test
    test

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
+import chalk from 'chalk';
+import { success, warning } from '@utils/logger/logger';
 import app from './app';
+
+if (!process.env.NODE_ENV) {
+  warning('NODE_ENV environment variable is missing');
+}
 
 const port = process.env.PORT || 5002;
 
 app.listen(port, () => {
-  console.log(`[OK] ${process.env.APP_NAME} is listening on port: ${port}`);
+  success(`${process.env.APP_NAME} is listening on port: ${chalk.green(port)}`);
 });
